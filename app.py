f2bce1b1c0426ae224b0c3d8eb95b955017e048c
PurgeTrapsCorp <kristof20011020@gmail.com>
2024-10-01 14:19:48 +0200

    app.py 8/8 fix

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from routes.UserDataRoutes import UserData_bp
 from routes.ResourcesRoutes import Resources_bp
 from routes.ScheduledWorkRoutes import ScheduledWork_bp
 from routes.ScheduledMaintenanceRoutes import ScheduledMaintenance_bp
+from routes.MaintanedResourcesRoutes import MaintanedResources_bp
+from routes.ScheduledResourcesRoutes import ScheduledResources_bp
 
 app = Flask(__name__)
 app.register_blueprint(ResourceTypes_bp)
@@ -13,6 +15,8 @@ app.register_blueprint(UserData_bp)
 app.register_blueprint(Resources_bp)
 app.register_blueprint(ScheduledWork_bp)
 app.register_blueprint(ScheduledMaintenance_bp)
+app.register_blueprint(MaintanedResources_bp)
+app.register_blueprint(ScheduledResources_bp)
 
 if __name__ == '__main__':
     app.run(port=6000, debug=True)
