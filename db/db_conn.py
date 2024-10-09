ecde9bffc0f6fb8b72a52ed22009c12cf9481da5
PurgeTrapsCorp <kristof20011020@gmail.com>
2024-10-09 20:58:40 +0200

    .venv update

--- db/db_conn.py
+++ db/db_conn.py
@@ -1,10 +1,17 @@
+import os
 import psycopg2
-import configparser
+from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read("./db/config.ini")
-password_input = config['configuration']['password']
-username_input = config['configuration']['username']
+load_dotenv()
+
+password_input = os.getenv('DB_PASSWORD')
+username_input = os.getenv('DB_USERNAME')
 
 def db_conn():
-    return psycopg2.connect(database="resourceManagement", host="localhost", user=username_input, password=password_input, port="5432")
+    return psycopg2.connect(
+        database="resourceManagement",
+        host="localhost",
+        user=username_input,
+        password=password_input,
+        port="5432"
+    )
