cb5f8b175a2c709e450e730c5b408f277ea87fa0
PurgeTrapsCorp <kristof20011020@gmail.com>
2024-10-10 16:25:47 +0200

    db update 2

--- db/db_conn.py
+++ db/db_conn.py
@@ -4,14 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+host_input = os.getenv('DB_HOST')
+port_input = os.getenv('DB_PORT')
+
 password_input = os.getenv('DB_PASSWORD')
 username_input = os.getenv('DB_USERNAME')
+
 
 def db_conn():
     return psycopg2.connect(
         database="resourceManagement",
-        host="localhost",
+        host=host_input,
         user=username_input,
         password=password_input,
-        port="5432"
+        port=port_input
     )
