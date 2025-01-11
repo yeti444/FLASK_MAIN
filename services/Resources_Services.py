88bc6909ac84cd08cb9cef8fc6eab621d4af9a46
PurgeTrapsCorp <kristof20011020@gmail.com>
2025-01-11 19:19:59 +0100

    post-mortem 0.1

--- services/Resources_Services.py
+++ services/Resources_Services.py
@@ -1,4 +1,7 @@
+import json
+
 from repository.Resources_Repository import get_all_Resources, get_one_Resources, create_Resources, update_Resources, delete_Resources
+
 
 def get_all_Resources_Service():
     return get_all_Resources()
@@ -7,10 +10,12 @@ def get_one_Resources_service(resourceId):
     return get_one_Resources(resourceId)
 
 def create_Resources_service(name, typeId, info):
-    return create_Resources(name, typeId, info)
+    info_json = json.dumps(info)
+    return create_Resources(name, typeId, info_json)
 
 def update_Resources_service(resourceId, name, typeId, info):
-    return update_Resources(resourceId, name, typeId, info)
+    info_json = json.dumps(info)
+    return update_Resources(resourceId, name, typeId, info_json)
 
 def delete_Resources_service(resourceId):
     return delete_Resources(resourceId)
