2fb0b3352002a83e505da744e899c8cdb1db8ced
PurgeTrapsCorp <kristof20011020@gmail.com>
2025-02-02 14:34:05 +0100

    post-mortem 0.5

--- controller/CheckAvailability_Routes.py
+++ controller/CheckAvailability_Routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from services.CheckAvailability_Services import check_time
+from services.CheckAvailability_Services import check_time, check_resource
 from services.Resources_Services import get_one_Resources_service
 
 from utils.utils import role_required
@@ -75,7 +75,7 @@ checkAvailability_bp = Blueprint('checkAvailability', __name__)
         }
     }
 })
-def get_MaintanedResources(resourceId):
+def get_isItAvailable(resourceId):
     date = request.args.get('date')
     interval = request.args.get('interval', "01:00:00")
     
@@ -87,5 +87,85 @@ def get_MaintanedResources(resourceId):
             return jsonify(result), 200
         else:
             return jsonify({'message': f'Resource with ID {resourceId} not found.'}), 404
+    except Exception as e:
+        return jsonify({'message': f'Internal Server Error: {str(e)}'}), 500
+
+@checkAvailability_bp.route('/api/checkAvailability', methods=['GET'])
+#@role_required(['Admin', 'User'])
+@swag_from({
+    'tags': ['checkAvailability'],
+    'description': 'List all Available Resources based on start date and duration.',
+    'parameters': [
+        {
+            'name': 'date',
+            'in': 'query',
+            'type': 'string',
+            'required': False,
+            'description': 'The date to check availability (format: YYYY-MM-DDTHH:MM:SS). Default is the current date and time.',
+            'schema': {
+                'type': 'string',
+                'example': '2024-10-20T00:00:00'
+            }
+        },
+        {
+            'name': 'interval',
+            'in': 'query',
+            'type': 'string',
+            'required': False,
+            'description': 'The time interval to check availability for (format: HH:MM:SS). Default is 01:00:00.',
+            'schema': {
+                'type': 'string',
+                'example': '24:00:00'
+            }
+        }
+    ],
+        'responses': {
+        '200': {
+            'description': 'The availability of the resource based on the given parameters.',
+            'examples': {
+                'application/json': {
+                    "availableResources": {
+                    "1": {
+                    "createDate": "Fri, 20 Sep 2024 12:48:39 GMT",
+                    "info": {
+                        "x": 100,
+                        "y": 300,
+                        "z": 400
+                    },
+                    "name": "test",
+                    "resourceId": 1,
+                    "typeId": 1
+                    }
+                }
+                }
+            }
+        },
+        '404': {
+            'description': 'Resource not found for the given resource ID.',
+            'examples': {
+                'application/json': {
+                    'message': 'Resource with the given ID not found.'
+                }
+            }
+        },
+        '500': {
+            'description': 'Internal Server Error, unexpected failure.',
+            'examples': {
+                'application/json': {
+                    'message': 'Internal Server Error: Something went wrong.'
+                }
+            }
+        }
+    }
+})
+def get_AvailableResources():
+    date = request.args.get('date')
+    interval = request.args.get('interval', "01:00:00")
+    
+    try:
+        result = check_resource(date, interval)
+        result_entries = {entry.resourceId: entry.to_dict() for entry in result}
+        return jsonify({'availableResources': result_entries}), 200
+
     except Exception as e:
         return jsonify({'message': f'Internal Server Error: {str(e)}'}), 500
--- services/CheckAvailability_Services.py
+++ services/CheckAvailability_Services.py
@@ -1,5 +1,6 @@
 from repository.MaintanedResources_Repository import get_all_MaintanedResources_time
 from repository.ScheduledResources_Repository import get_all_ScheduledResources_time
+from repository.Resources_Repository import get_all_Resources
 from utils.utils import is_overlap, string_to_datetime, string_to_timedelta
 
 def check_time(resourceId, fromDate, duration):
@@ -7,8 +8,6 @@ def check_time(resourceId, fromDate, duration):
                 
     S_Resources = get_all_ScheduledResources_time()
     M_Resources = get_all_MaintanedResources_time()
-
-    print(duration)
     
     datetime_fromDate = string_to_datetime(fromDate)
     timedelta_duration = string_to_timedelta(duration)
@@ -16,7 +15,7 @@ def check_time(resourceId, fromDate, duration):
 
     
     for single in S_Resources:
-        if single.resourceId is int(resourceId):
+        if single.resourceId == int(resourceId):
             if is_overlap(datetime_fromDate, timedelta_duration, single.fromdate, single.duration):
                 return {
                 "occupied": True,
@@ -25,7 +24,7 @@ def check_time(resourceId, fromDate, duration):
                 
     
     for single in M_Resources:
-        if single.resourceId is int(resourceId):
+        if single.resourceId == int(resourceId):
             if is_overlap(datetime_fromDate, timedelta_duration, single.fromdate, single.duration):
                 return {
                 "occupied": True,
@@ -36,5 +35,19 @@ def check_time(resourceId, fromDate, duration):
     "where": "nowhere"
     }
 
+def check_resource(fromDate, duration):
+    A_Resources = get_all_Resources()
+    F_Resources = []
+    
+    for res in A_Resources:
+        check_result = check_time(res.resourceId, fromDate, duration)
+        if not check_result["occupied"]:
+            F_Resources.append(res)
 
+    return F_Resources
+
+
+
+            
+    
 
